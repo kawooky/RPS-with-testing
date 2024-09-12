ba484b959128451fc6f1ced8bfdce7fa049b848e
kawooky <kawooky@gmail.com>
2024-09-12 14:03:09 +0100

    removed before each

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,18 +1,10 @@
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 
 class AppTest {
 
     private User user;
-
-    @BeforeEach
-    void setUp() {
-        user = new User();
-    }
 
     @Test
     void testUserSetName() {
